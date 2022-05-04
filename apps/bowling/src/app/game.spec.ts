d8b3ebda3df374425ae3411145b57bf2ad67dfd8
sclin87 <mikelolct.cs07@nctu.edu.tw>
2022-05-04 21:28:58 +0800

    Initial commit

---
+++ apps/bowling/src/app/game.spec.ts
@@ -0,0 +1,39 @@
+// game.spec.ts
+import { Game } from './game';
+
+describe('Game', () => {
+  let game: Game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
+  it('should create an instance', () => {
+    expect(game).toBeTruthy();
+  });
+
+  test('all zero', () => {
+    rollMany(20, 0);
+    expect(game.score).toBe(0);
+  });
+
+  test('all one', () => {
+    rollMany(20, 1);
+    expect(game.score).toBe(20);
+  });
+
+  // game.spec.ts
+  test('test on one spare', () => {
+    game.roll(5);
+    game.roll(5); // spare
+    game.roll(3);
+    rollMany(17, 0);
+    expect(game.score).toBe(16);
+  });
+
+  function rollMany(n: number, pins: number) {
+    for (let i = 0; i < n; i++) {
+      game.roll(pins);
+    }
+  }
+});
